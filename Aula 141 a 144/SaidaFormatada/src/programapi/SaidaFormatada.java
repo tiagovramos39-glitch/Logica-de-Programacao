297b811ec6703e23bd452e6150c73ac0a572c9a9
tiagovramos39-glitch <tiagovramos39@gmail.com>
2026-05-22 21:41:26 -0300

    Add files via upload

---
+++ Aula 141 a 144/SaidaFormatada/src/programapi/SaidaFormatada.java
@@ -0,0 +1,17 @@
+package programapi;
+
+public class SaidaFormatada {
+
+	public static void main(String[] args) {
+		String nome = "Tiago";
+		int idade = 26;
+		
+		System.out.printf("Meu nome é %s e tenho %d anos. \n\n", nome, idade);
+		
+		double pi = 3.1415926536;
+		
+		System.out.printf("O valor de pi é %.2f", pi);
+
+	}
+
+}
